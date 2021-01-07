674985f205d02098f7539ed63c198f011427b1c5
Konrad Creutzburg <creukonr>
2021-01-07 15:01:03 +0100

    comments

--- doubleToggle.ino
+++ doubleToggle.ino
@@ -1,16 +1,16 @@
 #include <avr/sleep.h>
 
 const int relayPin = 2;
-const int delayTime = 500;  //0,5sec
+const int delayTime = 500; //0,5sec
 
 void setup(){
-    delay(1000);
+    delay(1000);                         //to give my coffee machine a headstart
     pinMode(relayPin, OUTPUT);
-    digitalWrite(relayPin, HIGH);
-    doubleToggle(relayPin,delayTime);
+    digitalWrite(relayPin, HIGH);        //low lever trigger: active on low
+    doubleToggle(relayPin, delayTime);
 
     set_sleep_mode(SLEEP_MODE_PWR_DOWN); // choose power down mode
-    sleep_mode(); // sleep now!
+    sleep_mode();                        // sleep now!
 }
 
 void loop(){
